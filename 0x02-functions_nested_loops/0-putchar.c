9aa8abca15bd66301fdd7c92706bbd70b82c945a
Imad AZ <imadazizimad@gmail.com>
2023-03-21 07:01:24 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar
  *
- * Return: Always 0
+ * return : return 0 if success!
  */
 
-int main(void)
-{
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
-}  
+	int main()
+	{
+		_putchar("_");
+		_putchar("p");
+		_putchar("u");
+		_putchar("t");
+		_putchar("c");
+		_putchar("h");
+		_putchar("a");
+		_putchar("r");
+		_putchar("\n");
+		return 0;
+	}
