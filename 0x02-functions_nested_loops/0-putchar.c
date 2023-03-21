717576ed95e5bdaffe7f13154a9152ba404a5150
Imad AZ <imadazizimad@gmail.com>
2023-03-21 06:07:04 -0700

    header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-#include <main.h>
+#include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+        int main(void)
+        {
+                _putchar('H');
+                _putchar('o');
+                _putchar('l');
+                _putchar('b');
+                _putchar('e');
+                _putchar('r');
+                _putchar('t');
+                _putchar('o');
+                _putchar('n');
+                _putchar('\n');
 
-	int main(void)
-	{
-		char *sh = "_putchar";
-
-		while (*sh)
-		{
-			_putchar(*sh);
-			sh++;
-		}
-		_putchar('\n');
-
-		return (0);
-	}
+                return (0);
+        }
+~                              
