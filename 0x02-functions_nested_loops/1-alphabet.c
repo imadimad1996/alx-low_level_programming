aff4461b67699be1c1b691fbf82c53659b3147dd
Imad AZ <imadazizimad@gmail.com>
2023-03-21 11:58:41 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints alphabet 
+ *
+ * return : return 0 if success
+ */
+int main(void)
+{
+	char i;
+	for (i = "a", i<=z, i++)
+	{
+		print_alphabet(i);
+	}
+	return 0;
+}
